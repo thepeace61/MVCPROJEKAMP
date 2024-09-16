8427cfca9365eb5513c42ccf6f3d82219592e816
victus <victus@DESKTOP-HSBTLBF>
2024-09-16 10:43:45 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Repositroies/CategoryRepository.cs
@@ -0,0 +1,53 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositroies
+{
+    public class CategoryRepository : ICategoryDal
+    {
+        // bu yöntem çok doğru bir yöntem değildir.
+        Context c = new Context();
+        DbSet<Category> _object;
+        public void Delete(Category p)
+        {
+            _object.Remove(p);
+            c.SaveChanges();
+        }
+
+        public void Insert(Category p)
+        {
+            _object.Add(p);
+            c.SaveChanges();
+        }
+
+        public List<Category> List()
+        {
+            return _object.ToList();
+        }
+
+        public List<Category> List(Expression<Func<Category, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category p)
+        {
+            c.SaveChanges();
+        }
+    }
+}
+/*
+ ToList
+ Add
+ Remove
+ Find
+
+ */
---
+++ MVCPROJEKAMPİ/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPROJEKAMPİ.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Test2()
+        {
+            return View();
+        }
+           
+    }
+}
